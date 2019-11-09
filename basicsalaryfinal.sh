19b27911985fef6e9495b658acdd6ab35ec7ff1a
vasamvishal <54842886+vasamvishal@users.noreply.github.com>
2019-11-09 17:53:09 +0530

    changes echo files and whole program

--- basicsalaryfinal.sh
+++ basicsalaryfinal.sh
@@ -1,22 +1,4 @@
 #!/bin/bash -x
-rate_hours=20;
-hour=0; 
-random=$((RANDOM%3))
-echo $random
-if [ $random == 1 ]
-then
-         emprate=20;
-         empHrs=8;
-         salary=$(( $emprate * $empHrs ))
-         echo $salary
-         echo "nothing"
-       
-
-else
-        echo "not present"
-fi
-
-
 
 
 
