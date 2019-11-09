dc8aab8b7553f8d05c4ffb1137eab749736d144e
vasamvishal <54842886+vasamvishal@users.noreply.github.com>
2019-11-09 17:16:32 +0530

    Update basicsalaryfinal.sh

--- basicsalaryfinal.sh
+++ basicsalaryfinal.sh
@@ -5,6 +5,10 @@ random=$((RANDOM%3))
 echo $random
 if [ $random == 1 ]
 then
+         emprate=20;
+         empHrs=8;
+         salary=$(( $emprate * $empHrs ))
+         echo $salary
         echo "present"
 
 else
