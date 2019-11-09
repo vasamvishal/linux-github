8d4c2789973def0c86a146190170f92fcce0abd8
vasamvishal <54842886+vasamvishal@users.noreply.github.com>
2019-11-09 17:57:22 +0530

    updated  evrything

--- basicsalaryfinal.sh
+++ basicsalaryfinal.sh
@@ -1,5 +1,23 @@
 #!/bin/bash -x
 
+rate_hours=20;
+hour=0; 
+random=$((RANDOM%3))
+echo $random
+if [ $random == 1 ]
+then
+         emprate=20;
+         empHrs=8;
+         salary=$(( $emprate * $empHrs ))
+         echo $salary
+         echo "nothing"
+       
+
+else
+        echo "not present"
+fi
+
+
 
 
 
